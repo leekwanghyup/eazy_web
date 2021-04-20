ee09c43e61ab328eb635c7f68e70b0d7299c2bdd
leekwanghyup <leekwanghyup@naver.com>
2021-04-20 23:01:43 +0900

    서랍메뉴 예제 완성

---
+++ jQuery_07_drawer_side_menu_BASE/js/main.js
@@ -0,0 +1,27 @@
+// $aside.stop().animate({left:'0px'}, 300); // 버튼을 클릭하면 서랍이 열림 
+
+// A.addClass('b c'); // A에 'b'와 'c'클래스명 추가   
+// A.removeClass('b c');  
+// A.toggleClass('b');  // 클래스명 추가(클래스명이없다면)/삭제(클래스명이 있으면) 
+// A.hasClass('b') // 클래스 b가 존재하면 참 
+// A.attr('href', 'b') // A의 href 속성을 b로 변경
+
+$(function(){
+    var $aside = $('aside'); 
+    var $button = $aside.find('button'); 
+    var $duration = 700; 
+
+    console.log($button.find('img').attr('src')); 
+
+    $button.click(function(){
+        console.log("button click"); 
+         $aside.toggleClass('open'); 
+        if($aside.hasClass('open')){
+            $aside.stop().animate({left:'-70px'}, $duration,'easeOutBack'); // 서랍 열림 easeOutBack: Jquery UI, 덜컹거리는 느낌 
+            $button.find('img').attr('src', 'img/btn_close.png'); // close버튼으로 이미지 변경 
+        } else{
+            $aside.stop().animate({left:'-350px'}, $duration,'easeInBack');            
+            $button.find('img').attr('src', 'img/btn_open.png') 
+        }
+    })
+})
